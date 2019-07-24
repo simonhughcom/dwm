c1ab07f0ed04486c8b9b428c1c48832cca23cc86
simonhughcom <simonhughdev@gmail.com>
2019-07-24 04:44:04 +0100

    statuscolor

--- config.def.h
+++ config.def.h
@@ -21,6 +21,7 @@ static const char *colors[][3]      = {
 	/*					fg         bg          border   */
 	[SchemeNorm] =	 { col_gray3, col_gray1,  col_gray2 },
 	[SchemeSel]  =	 { col_gray4, col_cyan,   col_cyan },
+	[SchemeTitle]  = { col_gray4, col_cyan,  col_cyan  },
 	[SchemeWarn] =	 { col_black, col_yellow, col_red },
 	[SchemeUrgent]=	 { col_white, col_red,    col_red },
 };
