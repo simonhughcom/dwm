bcb3257941821269d6e3830901052416076e9d7e
simonhughcom <simonhughdev@gmail.com>
2019-07-24 14:05:20 +0100

    config

--- config.h
+++ config.h
@@ -1,13 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int gappx     = 8;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-/* static const char *fonts[]          = { "monospace:size=18" }; */
-static const char *fonts[]          = { "System San Francisco Display:size=18" };
+static const char *fonts[]          = { "monospace:size=18" };
+/* static const char *fonts[]          = { "System San Francisco Display:size=18" }; */
 static const char dmenufont[]       = "monospace:size=18";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
