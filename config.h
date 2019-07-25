2b3946392bf4045ebf2c2ddc671cdbb61d94be24
simonhughcom <simonhughdev@gmail.com>
2019-07-25 18:06:24 +0100

    changed glyphs for tags

--- config.h
+++ config.h
@@ -40,7 +40,7 @@ static const char *colors[][3]      = {
 
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "", "", "" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "📺", "📖", "📊" };
 
 static const Rule rules[] = {
 	/* xprop(1):
